f1d0a7be9633c2a1683b63355b7d27ac1dc44808
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-06-29 14:09:00 +0200

    Update app_urls.dart

--- lib/commons/app_urls.dart
+++ lib/commons/app_urls.dart
@@ -1,3 +1,5 @@
 class AppUrls {
-  static String baseUrl = "";
+  static String baseUrl = "https://apimanager.altervista.org";
+
+  static String shareTravelUrl = "$baseUrl/api/iTravel/share-travel.php";
 }
