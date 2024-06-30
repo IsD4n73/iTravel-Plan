ec7489695b5966c17c19d3e1c892842df6777af3
Dante <68009431+IsD4n73@users.noreply.github.com>
2024-06-30 22:46:16 +0200

    Update global_instance.dart

--- lib/commons/global_instance.dart
+++ lib/commons/global_instance.dart
@@ -5,6 +5,8 @@ import 'package:path_provider/path_provider.dart';
 class GlobalInstance {
   static late Box appDB;
 
+  static bool needUpdate = false;
+
   static initDB() async {
     var document = await getApplicationDocumentsDirectory();
     appDB = await Hive.openBox('iTravelDB', path: document.path);
